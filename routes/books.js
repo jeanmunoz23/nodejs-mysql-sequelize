4e0eef880bba805c50d18c8cb9930bbf8617eab1
jemprojects <jemprojectsgit@gmail.com>
2023-10-23 15:24:16 -0300

    revert

--- routes/books.js
+++ routes/books.js
@@ -1,30 +1,54 @@
-module.exports = (sequelize, Sequelize, DataTypes) => {
-  const Book = sequelize.define(
-    "book", // Model name
-    {
-      // Model attributes
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      title: {
-        type: DataTypes.STRING
-      },
-      author: {
-        type: DataTypes.STRING
-      },
-      published: {
-        type: DataTypes.BOOLEAN
-      },
-    },
-    {
-      // Options
-      timestamps: true,
-      underscrored: true,
-    }
-  );
+const express = require('express');
+const router = express.Router();
+const Book = require('../database/models/Book');
 
-  return Book;
-};
+// INDEX /api/Books
+router.get('/', (req, res) => {
+    Book.findAll().then(result => {
+        res.json(result);
+    })
+})
+
+// CREATE
+router.post('/', (req, res) => {
+    Book.create({
+        title: req.body.title,
+        body: req.body.body
+    }).then(result => {
+        res.json(result);
+    })
+});
+
+// READ /api/Books/:id
+router.get('/:id', (req, res) => {
+    Book.findByPk(req.params.id).then(result => {
+        res.json(result);
+    })
+});
+
+// UPDATE /api/Books/:id
+router.patch('/:id', (req, res) => {
+    Book.update({
+        title: req.body.title,
+        body: req.body.body
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(result => {
+        res.json(result);
+    });
+});
+
+// DELETE /api/Books/:id
+router.delete('/:id', (req, res) => {
+    Book.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(result => {
+        res.json(result);
+    })
+});
+
+module.exports = router;
